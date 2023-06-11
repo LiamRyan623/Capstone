273b174e897cd33e6a32e89767d3aad6814570f4
Will Mashrick <willmashrick@gmail.com>
2023-06-11 14:39:28 -0400

    Working on userLogin and userSignup. Want to use tabs in Auth component in order to toggle between signup and login.

--- client/src/components/auth/Auth.jsx
+++ client/src/components/auth/Auth.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import UserLogin from "./login/UserLogin";
 import UserSignup from "./signup/UserSignup";
+import { Nav, NavItem, NavLink, Col, Container } from "reactstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
   return (
     <>
-      Auth
-      <UserLogin />
-      <UserSignup />
+      <Col md="6">
+        <Nav justified pills tabs>
+          <NavItem>
+            <NavLink onClick={{}}>Login</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active>Signup</NavLink>
+          </NavItem>
+        </Nav>
+
+        <UserSignup />
+      </Col>
     </>
   );
 }
--- client/src/components/auth/login/UserLogin.jsx
+++ client/src/components/auth/login/UserLogin.jsx
@@ -1,5 +1,75 @@
-import React from "react";
+import { useRef } from "react";
+import { Form, FormGroup, Input, Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function UserLogin() {
-  return <>UserLogin</>;
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  // const navigate = useNavigate();
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   //console.log(emailRef.current.value);
+
+  //   // Build the body object for server endpoint
+  //   let body = JSON.stringify({
+  //     email: emailRef.current.value,
+  //     password: passwordRef.current.value,
+  //   });
+  //   // Declare and init our url
+  //   const url = "http://localhost:4005/user/login";
+
+  //   try {
+  //     const res = await fetch(url, {
+  //       method: "POST",
+  //       headers: new Headers({
+  //         "Content-Type": "application/json",
+  //       }),
+  //       body: body,
+  //     });
+  //     const data = await res.json();
+
+  //     if (data.message === "Login Successful!") {
+  //       updateToken(data.token);
+  //       navigate("/rooms");
+  //     } else {
+  //       alert(data.message);
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  return (
+    <>
+      <Form>
+        <FormGroup>
+          <Input
+            placeholder="Email"
+            innerRef={emailRef}
+            type="email"
+            autoComplete="off"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            placeholder="Password"
+            innerRef={passwordRef}
+            type="password"
+            autoComplete="off"
+          />
+        </FormGroup>
+        <Button
+          outline
+          color="primary"
+          style={{ float: "right" }}
+          type="submit"
+          block
+        >
+          Login
+        </Button>
+      </Form>
+    </>
+  );
 }
