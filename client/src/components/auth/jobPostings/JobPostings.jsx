c6183e54ee8c806169b7748b03250569ad019149
Will Mashrick <willmashrick@gmail.com>
2023-06-28 13:01:44 -0400

    Working on trying to get JobPostings.jsx to display two cards at random. Might have an issue with fetch.

--- client/src/components/auth/jobPostings/JobPostings.jsx
+++ client/src/components/auth/jobPostings/JobPostings.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import {
   Card,
   CardTitle,
@@ -12,7 +11,6 @@ import {
 } from "reactstrap";
 
 export default function JobPostings(props) {
-  const navigate = useNavigate();
   const [jobs, setJobs] = useState([]);
 
   const fetchJobs = async () => {
@@ -24,20 +22,17 @@ export default function JobPostings(props) {
       }),
     };
 
-    // Get all, random array of job objects, would need that to be able to get random. Have to pull random jobs. Tinder code?
-    // ! Google: pulling random index from array.
-    // How to get random - last step // Poss that you get two of the same job.. figure it out
     // ! Research tinder copy-cat!!!
     // Get-All jobs for array of jobs - pick random index from array - Kate say make a random number generator function - [;'conditionals so you aren't doing same number so no same jobs - easiest way to do this!!  jobs length -1 //! RESEARCH THIS
     try {
       const res = await fetch(url, requestOptions);
       const data = await res.json();
 
-      // if (data.status === "Job Found") {
-      setJobs(data.getAllJobs);
-      // } else {
-      //   setJobs([{ company: "Host", body: "Oops! No jobs." }]);
-      // }
+      if (data.status === "Job Found") {
+        setJobs(data.getAllJobs);
+      } else {
+        setJobs([{ company: "Host", body: "Oops! No jobs." }]);
+      }
     } catch (err) {
       console.log(err);
     }
@@ -53,27 +48,40 @@ export default function JobPostings(props) {
 
   return (
     <>
-      {jobs.map((job) => (
-        <CardGroup key={job._id}>
-          <Card>
-            <CardImg
-              alt="Card image cap"
-              src="https://picsum.photos/318/180"
-              top
-              width="100%"
-            />
-            <CardBody>
-              <CardTitle tag="h5">{job.job}</CardTitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                {job.company}
-              </CardSubtitle>
-              <CardText>{job.description}</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </CardGroup>
-      ))}
+      <CardGroup>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">{jobs[3].job}</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              {jobs[3].company}
+            </CardSubtitle>
+            <CardText>{jobs[3].description}</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">{jobs[2].job}</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              {jobs[2].company}
+            </CardSubtitle>
+            <CardText>{jobs[2].description}</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </CardGroup>
     </>
   );
-  // return <>{displayJobs}</>;
 }
